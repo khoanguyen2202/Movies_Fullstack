34412781c9b594400cffd1a2266230ef37f15b85
Khoa Nguyen <khoanguyen1799x@gmail.com>
2024-04-11 08:53:14 +0700

    Backend
    Folder movies for backend

---
+++ movies/src/test/java/dev/khoa/mthu/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.khoa.mthu.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
